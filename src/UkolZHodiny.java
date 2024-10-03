c6fe5c4499e6c376588a4c3567a611122b5151cc
lukas pavera <lukas.pavera@infis.cz>
2024-10-03 15:24:03 +0200

    kalkulacka a tak 2.0

---
+++ src/UkolZHodiny.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class UkolZHodiny {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        /*
+        //zjistovani jestli je cislo delitelne 5
+
+
+        System.out.println("Napiš číslo jesli je delitelné 5");
+        int a = sc.nextInt();
+
+        if (a % 5 == 0) {
+            System.out.println("číslo je dělitelné 5");
+        }else
+        System.out.println("číslo není dělitelné 5");
+        */
+
+        /*
+            //hazeni 2x kostkou a soucet vyssi nez 10
+
+
+        System.out.println("hod kostkou");
+
+        Random rng = new Random();
+        int HodHostkou = rng.nextInt(1, 7);
+        int HodKostkou = rng.nextInt(1, 7);
+
+        if (HodHostkou + HodKostkou == 10) {
+            System.out.println("Vyhráls");
+        } else {
+            System.out.println("Máš to blbý");
+        }
+        */
+
+        //zadani ciselne hodnoty a zjistit jestli je v intervalu
+
+
+        System.out.println("Napiš číslo");
+        int b = sc.nextInt();
+
+        if (100 >= b <= -100)
+
+
+    }
+}
