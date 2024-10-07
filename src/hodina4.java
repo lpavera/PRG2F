2693cb1339870374790ab199634dac4f7f909ce6
lpavera <lukas.pavera@infis.cz>
2024-10-07 12:43:23 +0200

    Add files via upload
    blabla

---
+++ src/hodina4.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class hodina4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("random");
+
+        Random rng = new Random();
+        int hodMinci = rng.nextInt(0, 2);
+        if (hodMinci == 1)
+            System.out.println("vyhral si");
+        else
+            System.out.println("si mrtvej");
+    }
+}
