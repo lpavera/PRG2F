07ec6b57fe2d68cd00c696990b90d7c5a992a010
lpavera <lukas.pavera@infis.cz>
2024-11-28 23:59:15 +0100

    uz mi jeblo

--- src/ukol1Stringy.java
+++ src/ukol1Stringy.java
@@ -11,6 +11,35 @@ public class ukol1Stringy {
         System.out.println("Zadejte příjmení");
         String prijmeni = sc.next();
 
+        String obracenejm = "";//proměné pro obrácena slova, uvozovky = prázdná proměná
+        String obraceneprij = "";
+
+        //přendavá do proměné obracenejm písmena z proměné jmeno pozpátku
+        //jmeno.length -1 ← -1 je kvuli tomu že číslování písmen ve slově začíná 0 ale délka je normalní
+        //i-- protože jdu od zádu
+        // i >=
+        for (int i = jmeno.length() - 1; i >= 0; i--) {
+            obracenejm += jmeno.charAt(i); // to znamená že se pomalu od zadu vypisuje to jmeno
+
+        }
+
+        for (int i = prijmeni.length() - 1; i >= 0; i--) {
+            obraceneprij += prijmeni.charAt(i); // alternativa: obraceneprij = obracenejm + prijmeni.charAt(i);
+        }
+
+        System.out.println("Jméno a příjmení pozpádku: " + obracenejm.toUpperCase() + " " + obraceneprij.toUpperCase());
+
+
+
+
+
+
+
+
+
+
+
+        /* tohle prej nesmim :((
 
         //na obracení pismen pozpatku
         String obracenejm = new StringBuilder(jmeno).reverse().toString();
@@ -18,5 +47,7 @@ public class ukol1Stringy {
 
         System.out.println("Vaše jméno a příjmení pozpátku:");
         System.out.println(obracenejm.toUpperCase() + " " + obraceneprij.toUpperCase());
+
+         */
     }
 }
